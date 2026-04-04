faf2bd1998cfcefdc9757c18c67a14d868671801
MataRocksbze <matarocksbzresort@outlook.com>
2026-04-04 06:59:08 -0600

    Revert monkey patch (module not found)

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,26 +4,6 @@
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
  */
 
-// 🐒 Monkey patch to add delay between Hygraph requests (fix rate limit)
-const { request: originalRequest } = require('gatsby-graphql-source-toolkit/dist/common/request.js');
-
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
-
-// Override the request function globally
-global.request = async (...args) => {
-  await delay(300); // 300ms delay = ~3.3 requests per second
-  return originalRequest(...args);
-};
-
-// Also patch the module export directly
-const requestModule = require('gatsby-graphql-source-toolkit/dist/common/request.js');
-requestModule.request = global.request;
-
-// Optional: log that patch is applied
-exports.onPreInit = () => {
-  console.log('🐒 Monkey patch applied: 300ms delay between Hygraph requests');
-};
-
 /**
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
