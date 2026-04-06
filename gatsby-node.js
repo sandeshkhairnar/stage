f10e6977611eaa36ce339c09ba88ce2ab53c1147
techcroft1 <sandeshkhairnar123@gmail.com>
2026-04-06 14:48:07 +0530

    feat: add GraphQL fragments for Hygraph content models and update gatsby-node configuration

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,23 +4,40 @@
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
  */
 
-const toolkit = require("gatsby-graphql-source-toolkit")
-const PQueue = require("p-queue").default || require("p-queue")
 
-const globalHygraphQueue = new PQueue({
-  concurrency: 1,
-  interval: 1000,
-  intervalCap: 4,
-})
+const nodeFetchModule = require("node-fetch")
+const originalFetch = nodeFetchModule.default || nodeFetchModule
 
-toolkit.wrapQueryExecutorWithQueue = (executor, options) => {
-  console.log(
-    `[Hygraph-Global-Throttler] Intercepting request queue for a model. Redirecting to singleton queue.`
-  )
-  return async (args) => {
-    return globalHygraphQueue.add(() => executor(args))
+const HYGRAPH_HOSTS = ["hygraph.com", "graphassets.com", "graphcms.com"]
+const MIN_INTERVAL_MS = 250 // 250 ms between requests = max 4 req/sec
+let lastRequestTime = 0
+
+const throttledFetch = async (url, options) => {
+  const isHygraph =
+    typeof url === "string" && HYGRAPH_HOSTS.some(h => url.includes(h))
+
+  if (isHygraph) {
+    const now = Date.now()
+    const wait = MIN_INTERVAL_MS - (now - lastRequestTime)
+    if (wait > 0) {
+      console.log(
+        `[Hygraph-Throttler] Waiting ${wait}ms before next request to stay under rate limit...`
+      )
+      await new Promise(r => setTimeout(r, wait))
+    }
+    lastRequestTime = Date.now()
   }
+
+  return originalFetch(url, options)
 }
+
+const cacheKey = require.resolve("node-fetch")
+require.cache[cacheKey].exports = throttledFetch
+require.cache[cacheKey].exports.default = throttledFetch
+
+// ---------------------------------------------------------------------------
+// GATSBY NODE APIs
+// ---------------------------------------------------------------------------
 
 /**
  * @type {import('gatsby').GatsbyNode['createPages']}
